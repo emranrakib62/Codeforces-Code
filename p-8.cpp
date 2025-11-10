2e9dd42d360c7eef94b98e246280201a447c6bfd
Emranul Hoque <emranrakib01862@gmail.com>
2025-11-10 16:45:59 +0600

    updated'

---
+++ p-8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    cout << "The Two Numbers Is" << endl;
+    cout << X << " " << Y << endl;
+    cout << "And After Reversing  ((((((((((((((((((((((((((($_$))))))))))))))))))))))" << endl;
+    cout << "The Solution Is <3 <3" << endl;
+    cout << Y << " " << X << endl;
+
+    return 0;
+}
+
