c24110ed916f29045d38aa7afe1162c7311f4563
Emranul Hoque <emranrakib01862@gmail.com>
2025-11-11 11:09:02 +0600

    updated

--- p-3.c
+++ p-3.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+   char N[20];
+   scanf("%s",N);
+   
+   int len=strlen(N);
+   printf("%c\n",N[len-3]); 
+    
+    
+    
+    return 0;
+}
