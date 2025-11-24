0d3c89813f8c6cfecd8b6053c9b1a6e747873320
Emranul Hoque <emranrakib01862@gmail.com>
2025-11-24 20:34:09 +0600

    updated

---
+++ p-35.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int T,N;
+    cin>>T>>N;
+    if(T>= N)
+        cout<<"Passed :)";
+    else
+        cout<<"Failed :(";
+    return 0;
+}
+
