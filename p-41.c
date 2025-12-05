4a70be4633ebd0fc69d656c7b9d87ef9002a4107
Emranul Hoque <emranrakib01862@gmail.com>
2025-12-05 18:13:18 +0600

    updated

---
+++ p-41.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int n,x=0;
+    char s[4];
+
+    scanf("%d", &n);
+    while(n--) {
+        scanf("%s", s);
+        if(strstr(s, "++") != NULL) {
+            x++;
+        } else {
+            x--;
+        }
+    }
+    printf("%d\n", x);
+    return 0;
+}
+
